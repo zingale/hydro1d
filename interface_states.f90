3a26d3221247f023ab52baaede3bc4a4f7dc255e
zingale <zingale>
2011-10-26 01:46:26 +0000

    add case for different slopes

--- interface_states.f90
+++ interface_states.f90
@@ -15,6 +15,24 @@ contains
     real (kind=dp_t),    intent(in   ) :: dt
 
 
+    select case (godunov_type)
+
+    case (0)
+       ! piecewise constant slopes
+
+    case (1)
+       ! piecewise linear slopes
+
+    case (2)
+       ! piecewise parabolic slopes
+       
+    case default
+       print *, "ERROR: invalid godunov_type"
+       stop
+
+    end select
+
+
   end subroutine make_interface_states
 
 end module interface_states_module
