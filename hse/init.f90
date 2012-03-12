d90bacaa0cf78a9d440a39b932e6d154bff6fe54
zingale <zingale>
2012-03-12 15:40:07 +0000

    add HSE problem

---
+++ hse/init.f90
@@ -0,0 +1,73 @@
+module init_module
+
+  use datatypes_module
+  use params_module
+  use probparams_module
+  use grid_module
+  use eos_module
+  use variables_module
+
+  implicit none
+
+  private
+
+  public :: init_data
+
+contains
+  
+  subroutine init_data(U)
+
+    type(gridvar_t), intent(inout) :: U
+
+    integer :: i
+    real (kind=dp_t) :: pres, pres_below, e, H
+
+
+    ! compute the pressure scale height (isothermal gas)
+    H = pres_base / dens_base / abs(grav)
+
+    print *, 'H = ', H
+    
+    ! first set the density profile
+    do i = U%grid%lo, U%grid%hi
+
+       if (do_isentropic) then
+          ! we can integrate HSE with p = K rho^gamma analytically
+          U%data(i,iudens) = dens_base*(grav*dens_base* &
+               (gamma - 1.0_dp_t)*U%grid%x(i)/ &
+               (gamma*pres_base) + 1.0_dp_t)**(1.0_dp_t/(gamma - 1.d0))
+          
+       else
+          ! the density of an isothermal gamma-law atm is exponential
+          U%data(i,iudens) = dens_base * exp(-U%grid%x(i)/H)
+          print *, i, U%data(i,iudens)
+       endif
+
+       U%data(i,iudens) = max(U%data(i,iudens),small_dens)
+    enddo
+
+    ! now determine the pressure via HSE and set the state by using the EOS
+    pres_below = pres_base
+    do i = U%grid%lo, U%grid%hi
+       if (U%data(i,iudens) > small_dens) then
+          pres = pres_below + &
+               0.5_dp_t*U%grid%dx*(U%data(i,iudens) + U%data(i-1,iudens)) * grav
+       else
+          pres = pres_below
+       endif
+
+       ! get the internal energy from the EOS
+       call eos(eos_input_p, pres, e, U%data(i,iudens))
+
+       ! store the remainder of the conserved state
+       U%data(i,iumomx) = 0.0_dp_t
+       U%data(i,iuener) = U%data(i,iudens)*e    ! no kinetic energy
+
+       pres_below = pres
+
+    enddo
+
+  end subroutine init_data
+
+end module init_module
+
---
+++ hse/probparams.f90
@@ -0,0 +1,64 @@
+!=============================================================================
+! probparams module
+!
+! The propparams module controls the runtime parameters for the
+! problem-specific initial conditions only.  It reads them in from
+! the same inputs file as the params module, but looks for a different
+! namelist.
+!=============================================================================
+
+module probparams_module
+
+  use datatypes_module
+  implicit none
+
+  !---------------------------------------------------------------------------
+  ! declare runtime parameters with SAVE attribute and default values
+  !---------------------------------------------------------------------------
+
+  real (kind=dp_t), save :: pres_base = 1.65e6_dp_t
+  real (kind=dp_t), save :: dens_base = 1.65e-3_dp_t
+  real (kind=dp_t), save :: small_dens = 1.e-7_dp_t
+  
+
+  logical, save :: do_isentropic = .false.
+
+  ! namelist
+  namelist /problem/ pres_base, dens_base, do_isentropic
+
+contains
+
+  !===========================================================================
+  ! init_params
+  !===========================================================================
+  subroutine init_probparams
+
+    use params_module, only: infile
+
+    integer :: lun, status
+
+    if (.not. infile == "") then
+
+       print *, 'reading problem-specific parameters from ', trim(infile)
+       
+       open(newunit=lun, file=trim(infile), status="old", action="read")
+       read(unit=lun, nml=problem, iostat=status)
+
+       if (status < 0) then
+          ! namelist doesn't exist.  Use the defaults and carry on.
+          continue
+
+       else if (status > 0) then
+          ! some problem with the namelist.
+          print *, "ERROR: problem namelist invalid"
+          stop
+
+       endif
+
+       close(unit=lun)
+
+    endif    
+
+  end subroutine init_probparams
+
+end module probparams_module
