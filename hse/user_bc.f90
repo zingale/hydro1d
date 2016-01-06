ebeeab291bec8e3c47d62f7a53be5796ff4d366b
Michael Zingale <michael.zingale@stonybrook.edu>
2016-01-06 16:02:49 -0500

    add user_bc_xm

--- hse/user_bc.f90
+++ hse/user_bc.f90
@@ -7,7 +7,17 @@ module user_bc_module
 
   implicit none
 
+  private
+
+  public :: user_bc_xm, user_bc_xp
+
 contains
+
+  subroutine user_bc_xm(U)
+
+    type(gridvar_t), intent(inout) :: U
+
+  end subroutine user_bc_xm
 
   subroutine user_bc_xp(U)
 
